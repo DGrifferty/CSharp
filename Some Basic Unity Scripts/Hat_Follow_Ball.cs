79d9fec03ccfbcce104aed2fee5a5b7e45443542
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2022-03-02 05:33:06 +0000

    Create Hat_Follow_Ball.cs

---
+++ Some Basic Unity Scripts/Hat_Follow_Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hat_Follow_Ball : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 Hat_offset;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + Hat_offset;
+    }
+}
